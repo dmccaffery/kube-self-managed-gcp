b25a303417287f5581ab765842126710d1411742
Deavon M. McCaffery <dmccaffery@users.noreply.github.com>
2020-10-19 00:17:47 +0100

    colorise the outputs

--- terraform.sh
+++ terraform.sh
@@ -19,9 +19,17 @@ if test "${tfargs#*destroy}" != "$tfargs"; then
   rm -f .terraform/terraform.tfstate 2>/dev/null || true
   rm -f errored.tfstate 2>/dev/null || true
 else
+  CLR_BRIGHT_GREEN='\033[1;32m'    # BRIGHT GREEN
+  CLR_BRIGHT_YELLOW='\033[1;33m'   # BRIGHT YELLOW
+  CLR_CLEAR='\033[0m'              # DEFAULT COLOR
   echo
-  echo "Use the following command to ssh into the management node:"
-  echo
+  echo "${CLR_BRIGHT_YELLOW}Use the following command to ssh into the management node:"
+  echo "${CLR_BRIGHT_GREEN}"
   terraform output ssh
+  echo "${CLR_CLEAR}"
   echo
+  echo "${CLR_BRIGHT_YELLOW}Use the following command to collect the kube config (after install kubernetes, of course):"
+  echo "${CLR_BRIGHT_GREEN}"
+  terraform output kube_config
+  echo "${CLR_CLEAR}"
 fi
